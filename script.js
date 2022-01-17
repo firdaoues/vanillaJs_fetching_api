f350d9b49e91e96dc9d291a9c5bdead1c37cc7ea
firdaoues <firdaouesmiraoui@gmail.com>
2022-01-17 14:11:33 +0100

    second commit

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let entries = [];
+let title = document.getElementById("title");
+
 /**
  * @desciption fetch piblicapis ....
  * @param {String} url
@@ -8,57 +11,44 @@ const fetchApi = async (url) => {
     const response = await fetch(url);
 
     const data = await response.json();
-    let title = document.getElementById("title");
-    let entries = data.entries;
-    let categories = data.entries.map((item) => {
-      return item.Category;
-    });
-    categories = [...new Set(categories)];
-    const categoriesSelection = document.getElementById("categories");
 
-    categories.map((category) => {
-      categoriesSelection.add(new Option(category));
-    });
-    const filteredEntries = entries.filter((item, index) => {
-      // debugger
-      return item.Category === "Animals";
-    });
+    entries = data.entries;
 
-    filteredEntries.map((item, index) => {
-      title.innerHTML += `<h1>${item.API}</h1>`;
-    });
+    // loadDataToHtml(entries);
+
+    loadCategoryDropDown(data);
   } catch (error) {
     console.error(error);
   }
-  // .THEN.CATCH promise
-  //   fetch("https://api.publicapis.org/entries")
-  //     .then(function (response) {
-  //       // The API call was successful!
-  //       if (response.ok) {
-  //         return response.json();
-  //       } else {
-  //         return Promise.reject(response);
-  //       }
-  //     })
-  //     .then(function (data) {
-  //       let entries = data.entries;
-  //       // This is the JSON from our response
-  //       console.log(data.entries);
-  //       let title = document.getElementById("title");
-
-  //       const filteredEntries = entries.filter((item, index) => {
-  //         return item.Category === "Animals";
-  //       });
-
-  //       filteredEntries.map((item, index) => {
-  //         title.innerHTML += `<h1>${item.API}</h1>`;
-  //       });
-  //     })
-  //     .catch(function (err) {
-  //       // There was an error
-  //       console.warn("Something went wrong.", err);
-  //     });
 };
+
+function loadCategoryDropDown(data) {
+  let categories = data.entries.map((item) => {
+    return item.Category;
+  });
+  categories = [...new Set(categories)];
+  const categoriesSelection = document.getElementById("categories");
+
+  categories.map((category) => {
+    categoriesSelection.add(new Option(category));
+  });
+  loadDataToHtml(entries);
+}
+
+function loadDataToHtml(dataToShow) {
+  title.innerHTML = "";
+
+  dataToShow.map((item, index) => {
+    title.innerHTML += `<h1>${item.API}</h1>`;
+  });
+}
+
+function onCategpryChange(val) {
+  filteredEntries = entries.filter((item, index) => {
+    return item.Category === val.value;
+  });
+  loadDataToHtml(filteredEntries);
+}
 
 async function init() {
   console.log("init");
@@ -69,3 +59,32 @@ async function init() {
 
 // }
 init();
+
+// .THEN.CATCH promise
+//   fetch("https://api.publicapis.org/entries")
+//     .then(function (response) {
+//       // The API call was successful!
+//       if (response.ok) {
+//         return response.json();
+//       } else {
+//         return Promise.reject(response);
+//       }
+//     })
+//     .then(function (data) {
+//       let entries = data.entries;
+//       // This is the JSON from our response
+//       console.log(data.entries);
+//       let title = document.getElementById("title");
+
+//       const filteredEntries = entries.filter((item, index) => {
+//         return item.Category === "Animals";
+//       });
+
+//       filteredEntries.map((item, index) => {
+//         title.innerHTML += `<h1>${item.API}</h1>`;
+//       });
+//     })
+//     .catch(function (err) {
+//       // There was an error
+//       console.warn("Something went wrong.", err);
+//     });
